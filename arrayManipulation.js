e8c973f95f4577c23e3a9900719931bfadac0167
unknown <kaaddison001@st.ug.edu.gh>
2024-05-19 10:31:39 +0100

    Completed  first task

---
+++ arrayManipulation.js
@@ -0,0 +1,20 @@
+
+
+
+ 
+ 
+
+function processArray(arr) {
+    return arr.map(num => {
+        if (num % 2 === 0) {
+            return num * num; 
+        } else {
+            return num * 3; 
+        }
+    });
+}
+
+
+const inputArray = [1, 2, 3, 4, 5];
+const resultArray = processArray(inputArray);
+console.log(resultArray); 
